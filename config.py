ebdbb7922ab18d658dca24ef11e49113fa42deb1
srivastavas08 <59473943+srivastavas08@users.noreply.github.com>
2020-07-02 15:28:48 +0530

    Final commit included performance test

--- config.py
+++ config.py
@@ -4,5 +4,9 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get(
         "SECRET_KEY") or b'jdncknzkcncomcozjvoksijvchcckcmm'
-
-    MONGODB_SETTINGS = {'db': 'Health_Care'}
+    MONGODB_SETTINGS = {
+    'db': 'Health_Care',
+    'host': 'mongodb+srv://cluster-sigma.f042j.gcp.mongodb.net/Health_Care',
+    'username':'mongoengine',
+    'password':'SHIvam7426'
+    }
