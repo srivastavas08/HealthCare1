a561cacda92f0684e8e0a14b2123c5a09b003547
srivastavas08 <59473943+srivastavas08@users.noreply.github.com>
2020-07-03 18:23:24 +0530

    Final Commit All Good

--- database_scripts/populate_db.sh
+++
@@ -1,6 +0,0 @@
-mongoimport --db HealthCare1 --collection master_diagnosis --file ./database_scripts/master_diagnosis.json --jsonArray
-mongoimport --db HealthCare1 --collection master_pharmacy --file ./database_scripts/master_pharmacy.json --jsonArray
-mongoimport --db HealthCare1 --collection new_patient --file ./database_scripts/new_patient.json --jsonArray
-mongoimport --db HealthCare1 --collection patient_diagnosis --file ./database_scripts/patient_diagnosis.json --jsonArray
-mongoimport --db HealthCare1 --collection patient_pharmacy --file ./database_scripts/patient_pharmacy.json --jsonArray
-
